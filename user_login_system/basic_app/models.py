5287a7ce94d84cf22d7f10f7c709952e35ea3f34
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-01-16 07:24:24 +0600

    21 Days Challenge (Day 16)

--- user_login_system/basic_app/models.py
+++ user_login_system/basic_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User    #Default user model imported from user
 # Create your models here.
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    #Additional
+    portfolio_site = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pic',blank=True)
+
+    def __str__(self):
+        return self.user.username
